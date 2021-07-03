57a488ac0359e4ddf38d8769037ba737912ce7dd
Paisan <prietbroek@eyecontrol.nl>
2021-07-03 11:03:16 +0200

    Display a list

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,17 @@
+import { Hero } from "./hero";
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Leo Messi' },
+    { id: 12, name: 'Luka Modric' },
+    { id: 13, name: 'Andres Iniesta' },
+    { id: 14, name: 'Mesut Ozil' },
+    { id: 15, name: 'Angel Di Maria' },
+    { id: 16, name: 'Riyad Mahrez' },
+    { id: 17, name: 'Frenkie de Jong' },
+    { id: 18, name: 'Sergio Busquets' },
+    { id: 19, name: 'Hernandez Xavi' },
+    { id: 20, name: 'Robert Lewandowski' },
+    { id: 21, name: 'Marco Reus' },
+    { id: 22, name: 'Kevin de Bruyne' },
+
+]
